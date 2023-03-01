b246c0ef6db936e9441f8ef0ce4d898c2fa16504
DouglasLindahl <douglas.lindahl@gmail.com>
2023-03-01 14:23:47 +0100

    posts are now displayed with the title, author, and description on the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,6 @@
+@php( $blogPosts = App\models\BlogPost::all())
+@php( $users = App\models\user::all())
+
 <h1>Welcome, {{$user["username"]}}</h1>
 
 @include('errors')
@@ -7,3 +10,13 @@
 </form>
 
 <a href="/">Log out</a>
+
+<div>
+    @foreach($blogPosts as $post)
+    <div class = "blogPost">
+        <h1>{{$post->title}}</h1>
+        <p>By: {{ App\models\user::find($post->user_id)->username }}</p>
+        <p>{{$post->content}}</p>
+    </div>
+    @endforeach
+</div>
